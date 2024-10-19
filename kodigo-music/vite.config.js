b608b3f76b9521cea3277210406dd3d663797e60
dev-rafael-arevalo <rafael.edgardo.arevalo@gmail.com>
2024-10-19 16:37:29 -0600

    regresando a dev

--- kodigo-music/vite.config.js
+++ kodigo-music/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kodigo-music/', // Asegúrate de que esta línea esté presente
-  build: {
-    outDir: 'dist', // La carpeta donde se generará la build
-  },
-});
+})
